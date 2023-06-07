2b49dea3b971a0fb1201edc7b4adb40d8bd40adc
ommamakhan <123452092+ommamakhan@users.noreply.github.com>
2023-06-07 14:20:35 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,11 @@
+# Simple interest in python
 
+Amount = int(input("Enter the amount: "))
+# Enter the number of years
+Year = int(input("Enter the number of years: "))
+# Enter the rate of interest
+Rate = float(input("Enter the rate of interest: "))
+
+SimpleInterset = (Amount*Year*Rate)/100
+
+print("The simple interset is:", SimpleInterset)
